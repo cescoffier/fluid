9fd776f624fae17142169cec83cd9b85cea35070
Clement Escoffier <clement.escoffier@gmail.com>
2017-10-22 14:04:44 +0200

    Just some notes

--- fluid-core/src/test/java/me/escoffier/fluid/core/sensor/ProcessorA.java
+++ fluid-core/src/test/java/me/escoffier/fluid/core/sensor/ProcessorA.java
@@ -12,8 +12,41 @@ public class ProcessorA extends Processor {
 
     @Override
     public void process() {
+
+        /*
+        
+            join(sensor-a, sensor-b)
+                .....
+
+                from(sensor-a)
+                    .mergeWith(sensorB)
+                    .transformWith(...)
+                    .to("average")
+
+            from("sensor-a")
+            .transformWith(instance)
+                .transformWith(x -> {   Function<Flowable, Flowable> wrapped into a "FluidFlow"
+                   return x
+                        .mergeWith(from("B))
+                        .map(json -> json.getDouble("value"))
+                        .window(10)
+                        .flatMap(MathFlowable::averageDouble);
+                 })
+                .transformWith(x -> { .... })
+            .to("result")
+
+
+            public class InstanceClass {
+                @Processor
+                public void process(@Input("sensor-a") input1, @Input("sensor-b") input2, @Output("average") output) {
+                    //....
+                }
+            }
+         */
+
+
         Flowable<JsonObject> measuresFromA = from("sensor-A");
-        Flowable<JsonObject> measuresFromB =from("sensor-B");
+        Flowable<JsonObject> measuresFromB = from("sensor-B");
 
         Flowable<Double> result = measuresFromA
             .mergeWith(measuresFromB)
