3fb8c34ef9654937ff67b6c45d5f1d5824d0a70c
Clement Escoffier <clement.escoffier@gmail.com>
2017-12-03 15:26:15 +0100

    Mark methods as “unchecked”

--- fluid-constructs/src/main/java/me/escoffier/fluid/constructs/CommonHeaders.java
+++ fluid-constructs/src/main/java/me/escoffier/fluid/constructs/CommonHeaders.java
@@ -9,20 +9,24 @@ public final class CommonHeaders {
   public static final String ORIGINAL = "fluid.original";
 
   private CommonHeaders() {
+    // Avoid direct instantiation.
   }
 
   public static String key(Data data) {
     return (String) data.get(KEY);
   }
 
+  @SuppressWarnings("unchecked")
   public static Optional<String> keyOpt(Data data) {
     return data.getOpt(KEY);
   }
 
+  @SuppressWarnings("unchecked")
   public static <T> T original(Data data) {
     return (T) data.get(ORIGINAL);
   }
 
+  @SuppressWarnings("unchecked")
   public static <T> Optional<T> originalOpt(Data data) {
     return data.getOpt(ORIGINAL);
   }
