b4b50ce1de4dd24b733a36ab49c6f45faa0210eb
Clement Escoffier <clement.escoffier@gmail.com>
2018-05-02 20:57:11 +0200

    Fix annotation

--- examples/review-example/src/main/java/me/escoffier/fluid/examples/review/FraudDetector.java
+++ examples/review-example/src/main/java/me/escoffier/fluid/examples/review/FraudDetector.java
@@ -2,6 +2,7 @@ package me.escoffier.fluid.examples.review;
 
 import io.vertx.core.json.JsonObject;
 import me.escoffier.fluid.annotations.Inbound;
+import me.escoffier.fluid.annotations.Outbound;
 import me.escoffier.fluid.annotations.Transformation;
 import me.escoffier.fluid.models.Message;
 import me.escoffier.fluid.models.Pair;
@@ -16,10 +17,10 @@ public class FraudDetector {
   @Inbound("movies")
   Source<JsonObject> reviews;
 
-  @Inbound("reviews")
+  @Outbound("reviews")
   Sink<JsonObject> valid;
 
-  @Inbound("fraud")
+  @Outbound("fraud")
   Sink<JsonObject> fraud;
 
   @Transformation
